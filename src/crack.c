26aaeb55f2ab0b8704fe42ceaab239ac0ce773fa
Fook Athena <fookathe@mi-m1.msk.21-school.ru>
2023-10-26 21:53:30 +0300

    Done 7 quest

---
+++ src/crack.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float x,y;
+    if (scanf("%f%f", &x, &y) == 2) {
+        if (x*x + y*y < 25) {
+            printf("GOTCHA\n");
+        } else {
+            printf("MISS\n");
+        }
+    } else {
+        printf("n/a\n");
+    }
+}
