5a29c34441b5e58a092df8827515bbe360cf121e
Fook Athena <fookathe@mi-m1.msk.21-school.ru>
2023-10-26 21:11:21 +0300

    Done 5 quest

--- src/important_function.c
+++ src/important_function.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-const e = 2.71828182845904523536;
-
-
 float expon(float x, float y) {
     return x*pow(10.0,y);
 }
@@ -12,6 +9,10 @@ int main() {
     float x;
     if (scanf("%f", &x) == 1) {
         float result = expon(7.0,-3.0)*pow(x,4.0)+((22.8 * pow(x,(1.0/3.0)) - expon(1.0, 3.0)) * x + 3) / (x * x / 2.0) - x * pow((10.0+x),(2.0/x)) - 1.01;
+        if (isnan(result)) {
+            printf("n/a\n");
+            return 0;
+        }
         printf("%.1f\n", result);
     } else {
         printf("n/a\n");
