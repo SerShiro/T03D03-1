54f487312de0c39e46e391877d92be115c5e4f95
Fook Athena <fookathe@mi-d2.msk.21-school.ru>
2023-10-26 18:00:44 +0300

    Fixed quest 2

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-int num1, num2;
+int num1;
+float num2;
 int main() {
-    if((scanf("%d%d", &num1, &num2) == 2)) {
-        if (num2 == 0) {
-            printf("%d %d %d n/a\n", num1+num2, num1-num2, num1*num2);
+    if((scanf("%d%f", &num1, &num2) == 2) && num2 == (float)((int)num2)) {
+        int newNum = (int)num2;
+        if (newNum == 0) {
+            printf("%d %d %d n/a\n", num1+newNum, num1-newNum, num1*newNum);
         } else {
-            printf("%d %d %d %d\n", num1+num2, num1-num2, num1*num2, num1/num2);
+            printf("%d %d %d %d\n", num1+newNum, num1-newNum, num1*newNum, num1/newNum);
         }
     } else {
         printf("n/a\n");
