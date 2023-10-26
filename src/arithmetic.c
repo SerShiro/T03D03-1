a6788b61bfbafc687bfa0e2bc75bb8f79f69e142
Fook Athena <fookathe@mi-m1.msk.21-school.ru>
2023-10-26 22:05:08 +0300

    Fixed bug

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -6,12 +6,12 @@ int main() {
     if((scanf("%d%f", &num1, &num2) == 2) && num2 == (float)((int)num2)) {
         int newNum = (int)num2;
         if (newNum == 0) {
-            printf("%d %d %d n/a\n", num1+newNum, num1-newNum, num1*newNum);
+            printf("%d %d %d n/a", num1+newNum, num1-newNum, num1*newNum);
         } else {
-            printf("%d %d %d %d\n", num1+newNum, num1-newNum, num1*newNum, num1/newNum);
+            printf("%d %d %d %d", num1+newNum, num1-newNum, num1*newNum, num1/newNum);
         }
     } else {
-        printf("n/a\n");
+        printf("n/a");
     }
 
     return 0;
